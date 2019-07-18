e7caad7e89c8e70287970c5d55c26eac69752f08
denironyx <denironyx@gmail.com>
2019-07-18 11:30:29 +0100

    Rename the sampleRun to AKure and create a aermod.R script

---
+++ Akure/aermod.R
@@ -0,0 +1,6 @@
+library(aermod)
+
+setwd("C:/Users/LENOVO/Documents/Air-pollution-Dispersion")
+
+## Runnig aermod script
+run_aermod(input = "Akure/aermod.inp", out_file = "Akure/aermod.out", exe_folder = "Akure")
